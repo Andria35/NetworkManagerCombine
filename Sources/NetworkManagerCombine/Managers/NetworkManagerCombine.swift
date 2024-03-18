e141a792d7b269c9e7fa1064bcb248d2d0ef8152
Andria <andria.inas@gmail.com>
2024-03-18 21:48:08 +0400

    cleanup code

--- Sources/NetworkManagerCombine/Managers/NetworkManagerCombine.swift
+++ Sources/NetworkManagerCombine/Managers/NetworkManagerCombine.swift
@@ -1,7 +1,6 @@
 import Foundation
 import Combine
 
-// ----------------------------------------
 public protocol APIClient {
     associatedtype EndpointType: APIEndpoint
     func request<T: Decodable>(_ endpoint: EndpointType) -> AnyPublisher<T, Error>
